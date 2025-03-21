7f0d5d5c460e518ba276e3372f5895ea417274ca
SrushtiSutar1702 <srushtisutar1702@gmail.com>
2025-03-21 10:13:09 +0530

    Update gitexpt.py

--- gitexpt.py
+++ gitexpt.py
@@ -3,3 +3,6 @@ num2=int(input("Enter number"))
 addition=num1+num2
 print("Addition of numbers:",addition)
 print("This is addition")
+mul=num1*num2
+print("Multiplication Is:",mul)
+print("Here is the multiplication")
